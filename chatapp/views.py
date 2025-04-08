cf00ede359743f5598fd447a7f7f4d87ad5f031a
LeonardAzah <isongleonard@gmail.com>
2025-04-08 06:04:54 +0100

    store and retrive messages from the databasse

--- chatapp/views.py
+++ chatapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import ChatRoom
+from pyexpat.errors import messages
+
+from .models import ChatRoom, ChatMessage
 
 
 # Create your views here.
@@ -9,4 +11,5 @@ def index(request):
 
 def chatroom(request, slug):
     chatroom = ChatRoom.objects.get(slug=slug)
-    return render(request, 'chatapp/room.html', {"chatroom": chatroom})
+    messages = ChatMessage.objects.filter(room=chatroom)[0:30]
+    return render(request, 'chatapp/room.html', {"chatroom": chatroom, "messages":messages})
